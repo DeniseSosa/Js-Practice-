93d446ce2835bdfb66475aa0681bc97331a05436
unknown <rocio.sosa94@gmail.com>
2024-07-25 16:49:37 -0300

    localstorage

--- src/app.js
+++ src/app.js
@@ -2,23 +2,34 @@ const inputTask = document.getElementById("input-task");
 const taskList = document.getElementById("draggableUl"); // ul
 const dropZone = document.getElementById("dropzone");
 let originalIndexLi = {}
-let listaStorage= []
+let listaStorage= JSON.parse(localStorage.getItem("listaStorage")) || []
 const textareaTask= document.getElementById("textareaTask")
 
 const openModal= document.querySelector("#open-modal")
 const closeModal= document.querySelector("#close-modal")
 const modal = document.querySelector("#modal-container")
 
+document.addEventListener("DOMContentLoaded", loadTasks);
+
 function addTask() {
   console.log(textareaTask.value);
   if (textareaTask.value === "") {
     alert("Debes escribir una tarea");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = textareaTask.value;
+    let task = textareaTask.value
+    listaStorage.push(task)
+    localStorage.setItem("listaStorage", JSON.stringify(listaStorage))
+    createLiTaskElement(task)
+    modal.classList.remove("mostrarModal")  
+  }
+      textareaTask.value=""
+}
+
+function createLiTaskElement(task){
+  let li = document.createElement("li");
+    li.innerHTML = task
     taskList.appendChild(li);
     li.classList.add("list"); ///Le agrego class="list" a <li>
-    
 
     let uniqueId= Date.now()- (Math.floor(Math.random()*38476)+1000)
     li.setAttribute("id", uniqueId);
@@ -31,9 +42,8 @@ function addTask() {
 
     buttonCheck.addEventListener("click", function () {
         // Obtener el elemento 'li' correspondiente usando el parentNode
-  const li = this.parentNode;
-
-  li.classList.toggle("toggle");
+      const li = this.parentNode;
+      li.classList.toggle("toggle");
     });
 
     let buttonTrash = document.createElement("button");
@@ -42,6 +52,8 @@ function addTask() {
 
     buttonTrash.addEventListener("click", function () {
       taskList.removeChild(li);
+      listaStorage = listaStorage.filter((t)=> t !== task)
+      localStorage.setItem("listaStorage", JSON.stringify(listaStorage))
     });
 
     let buttonRevert = document.createElement("button");
@@ -71,12 +83,7 @@ function addTask() {
         sourceData= document.getElementById(getId)
         if(sourceData) dropZone.appendChild(sourceData)
       })
-      modal.classList.remove("mostrarModal")  
-  }
-
-  textareaTask.value = "";
 }
-
 
 
 
@@ -121,5 +128,9 @@ const form = document.querySelector(".formModal")
 form.addEventListener("click", function(event){
   event.preventDefault()
 })
+
+function loadTasks() {
+  listaStorage.forEach((task) => createLiTaskElement(task));
+}
 
 
